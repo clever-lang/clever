9316950a3bb0fd809b956939e956724cbd9274ff
Felipe Pena <felipensp@gmail.com>
2011-01-16 18:45:41 +0000

    - Removed commented code

--- include/symboltable.h
+++ include/symboltable.h
@@ -41,7 +41,7 @@ class Scope;
 typedef boost::unordered_map<const CString*, Value*> ScopeBase;
 typedef std::deque<Scope> ScopeManagerBase;
 
-class Scope: public ScopeBase {
+class Scope : public ScopeBase {
 public:
 	Scope() { }
 	~Scope() {
@@ -76,7 +76,7 @@ public:
 	}
 };
 
-class ScopeManager: public ScopeManagerBase {
+class ScopeManager : public ScopeManagerBase {
 public:
 	ScopeManager() : m_scope(-1) { }
 
@@ -132,68 +132,7 @@ private:
 		return NULL;
 	}
 };
-/*
-class SymbolTable {
-public:
-	typedef boost::unordered_map<const CString*, Value*> var_map;
-	typedef std::deque<var_map> var_scope;
 
-	SymbolTable()
-		: m_var_at(-1) { }
-
-	void register_var(Value* var, Value* value) throw() {
-		m_variables.at(m_var_at).insert(std::pair<CString*, Value*>(var->getStringP(), value));
-		var->set_value(value);
-	}
-
-	Value* get_var(Value* var) throw() {
-		Value* value = var->isNamedValue() ? var->get_value() : NULL;
-		CString* name;
-
-		if (value) {
-			return value;
-		}
-
-		name = var->getStringP();
-
-		* Searchs for the variable in the inner and out scopes *
-		for (int i = m_var_at; i >= 0; --i) {
-			var_map::iterator it = m_variables.at(i).find(name);
-
-			if (it != m_variables.at(i).end()) {
-				var->set_value(it->second);
-				return it->second;
-			}
-		}
-		return NULL;
-	}
-
-	void pushVarMap(var_map map) throw() {
-		m_variables.push_back(map);
-		++m_var_at;
-	}
-
-	var_map& topVarMap() throw() {
-		return m_variables.at(m_var_at);
-	}
-
-	void popVarMap() throw() {
-		var_map::iterator it = topVarMap().begin();
-
-		while (it != topVarMap().end()) {
-			it->second->delRef();
-			++it;
-		}
-		m_variables.pop_back();
-		--m_var_at;
-	}
-
-	DISALLOW_COPY_AND_ASSIGN(SymbolTable);
-private:
-	var_scope m_variables;
-	int m_var_at;
-};
-*/
 } // clever
 
 #endif // CLEVER_SYMBOLTABLE_H
