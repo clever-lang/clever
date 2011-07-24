5da0b48a0f233620ef43154db39080fd3e8a92b0
Felipe Pena <felipensp@gmail.com>
2011-07-23 23:48:23 -0300

    - Change header comment

--- include/pkgmanager.h
+++ include/pkgmanager.h
@@ -1,4 +1,4 @@
-/*
+/**
  * Clever programming language
  * Copyright (c) 2011 Clever Team
  *
@@ -21,8 +21,6 @@
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
- *
- * $Id$
  */
 
 #ifndef CLEVER_PKGMANAGER_H
--- src/astvisitor.cc
+++ src/astvisitor.cc
@@ -1,4 +1,4 @@
-/*
+/**
  * Clever programming language
  * Copyright (c) 2011 Clever Team
  *
@@ -21,8 +21,6 @@
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
- *
- * $Id: vm.h 341 2011-01-22 14:07:26Z felipensp $
  */
 
 #include "ast.h"
