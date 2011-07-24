e9cfe6bbbd5e615f30b78cb5285f0222eb1896a9
Ricardo Gomes <cadopoa@gmail.com>
2011-07-24 18:02:12 -0300

    - Added missing string_type files.

---
+++ include/string_type.h
@@ -0,0 +1,51 @@
+/**
+ * Clever programming language
+ * Copyright (c) 2011 Clever Team
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef CLEVER_STRING_H
+#define CLEVER_STRING_H
+
+#include "type.h"
+#include "value.h"
+
+namespace clever {
+
+class String : public Type {
+public:
+	String() :
+		Type("String") { }
+
+	void Init();
+
+	/* Type methods */
+	static CLEVER_TYPE_METHOD(substring);
+	static CLEVER_TYPE_METHOD(toFloat);
+	static CLEVER_TYPE_METHOD(toInteger);
+private:
+	DISALLOW_COPY_AND_ASSIGN(String);
+};
+
+} // clever
+
+#endif // CLEVER_STRING_H
---
+++ src/string_type.cc
@@ -0,0 +1,91 @@
+/**
+ * Clever programming language
+ * Copyright (c) 2011 Clever Team
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
+#include <stdexcept>
+#include "cstring.h"
+#include "type.h"
+#include "string_type.h"
+
+namespace clever {
+
+/**
+ * String::substring()
+ * Retrieves a substring from the original one.
+ */
+CLEVER_TYPE_METHOD(String::substring) {
+	if (args->at(0)->getInteger() >= value->toString().length()) {
+		std::cerr << "Out of range: " << args->at(0)->getInteger() << " is after the end of the string." << std::endl;
+		std::exit(1);
+	}
+
+	std::string substr = value->toString().substr(args->at(0)->getInteger(), args->at(1)->getInteger());
+	retval->setString(CSTRING(substr));
+	retval->set_type(Value::STRING);
+}
+
+/**
+ * String::toDouble()
+ * Converts the string to a float, if possible.
+ */
+CLEVER_TYPE_METHOD(String::toFloat) {
+	double floatValue;
+	std::stringstream stream(value->toString());
+
+	if ((stream >> floatValue).fail()) {
+		std::cerr << "\"" << value->toString() << "\" is not a valid float." << std::endl;
+		std::exit(1);
+	}
+
+	retval->setDouble(floatValue);
+	retval->set_type(Value::DOUBLE);
+}
+
+/**
+ * String::toInteger()
+ * Converts the string to an integer, if possible.
+ */
+CLEVER_TYPE_METHOD(String::toInteger) {
+	int64_t integer;
+	std::stringstream stream(value->toString());
+
+	if ((stream >> integer).fail()) {
+		std::cerr << "\"" << value->toString() << "\" is not a valid integer." << std::endl;
+		std::exit(1);
+	}
+
+	retval->setInteger(integer);
+	retval->set_type(Value::INTEGER);
+}
+
+void String::Init() {
+	addMethod(new Method("substring", (MethodPtr)&String::substring));
+	addMethod(new Method("toFloat", (MethodPtr)&String::toFloat));
+	addMethod(new Method("toInteger", (MethodPtr)&String::toInteger));
+}
+
+} // clever
