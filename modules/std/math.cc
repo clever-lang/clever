f072a062280e3300220181004dd2c9393ca4662c
Felipe Pena <felipensp@gmail.com>
2011-07-24 22:48:03 -0300

    - Update header comment

--- modules/std/math.cc
+++ modules/std/math.cc
@@ -1,4 +1,4 @@
-/*
+/**
  * Clever programming language
  * Copyright (c) 2011 Clever Team
  *
@@ -22,7 +22,6 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
  *
- * $Id: std.cc 299 2011-01-16 23:06:11Z felipensp $
  */
 
 #include <cmath>
--- modules/std/std_pkg.cc
+++ modules/std/std_pkg.cc
@@ -1,4 +1,4 @@
-/*
+/**
  * Clever programming language
  * Copyright (c) 2011 Clever Team
  *
@@ -21,8 +21,6 @@
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
- *
- * $Id: compiler.cc 315 2011-01-18 23:24:26Z felipensp $
  */
 
 #include "compiler/cstring.h"
