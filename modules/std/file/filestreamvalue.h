16f839ce4cc826c03a7246b920716cd59ee2340c
Felipe Pena <felipensp@gmail.com>
2011-07-25 23:50:50 -0300

    - Cosmetics

--- modules/std/file/filestreamvalue.h
+++ modules/std/file/filestreamvalue.h
@@ -1,4 +1,16 @@
-/* The above copyright notice and this permission notice shall be
+/**
+ * Clever programming language
+ * Copyright (c) 2011 Clever Team
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
  *
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
@@ -9,13 +21,14 @@
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
- * $Id$
  */
 
 #ifndef CLEVER_FILESTREAMVALUE_H
 #define CLEVER_FILESTREAMVALUE_H
 #include <fstream>
 #include "compiler/datavalue.h"
+
+namespace clever { namespace packages { namespace std { namespace file {
 
 struct FileStreamValue : public DataValue
 {
@@ -29,5 +42,7 @@ struct FileStreamValue : public DataValue
 	
 	~FileStreamValue() {}
 };
+
+}}}} // clever::packages::std::file
 
 #endif
