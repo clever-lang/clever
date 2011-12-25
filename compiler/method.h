9915b078bc53fdb0730239b6c4081cd67ab11a5b
Murilo Adriano Vasconcelos <muriloufg@gmail.com>
2011-12-25 19:17:59 -0200

    Rerefactioring method calls

--- compiler/method.h
+++ compiler/method.h
@@ -42,6 +42,7 @@ typedef std::vector<const Type*> TypeVector;
 #define CLEVER_METHOD_ARGS const ValueVector* args, Value* retval, Value* value, Method* clv_method_
 #define CLEVER_METHOD_NAME(name) clv_m_##name
 #define CLEVER_METHOD(name) void CLEVER_FASTCALL name(CLEVER_METHOD_ARGS) throw()
+#define CLEVER_ARGS_SEPARATOR '#'
 
 typedef void (CLEVER_FASTCALL *MethodPtr)(CLEVER_METHOD_ARGS);
 
