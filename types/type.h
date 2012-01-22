dacedcfc937d6493e4d91739de4a46150343d01a
Felipe Pena <felipensp@gmail.com>
2012-01-22 19:47:43 -0200

    - Split complex methods into a .cc file

--- types/type.h
+++ types/type.h
@@ -119,68 +119,11 @@ public:
 	explicit Type(const CString* name)
 		: RefCounted(1), m_name(name) { }
 
-	virtual ~Type() {
-		MethodMap::const_iterator it = m_methods.begin(), end = m_methods.end();
+	virtual ~Type();
 
-		while (it != end) {
-			OverloadMethodMap::const_iterator be = it->second.begin(),
-				ed = it->second.end();
+	void addMethod(Method*);
 
-			while (be != ed) {
-				be->second->delRef();
-				++be;
-			}
-
-			++it;
-		}
-	}
-
-	void addMethod(Method* method) {
-		const MethodArgs& args = method->getArgs();
-		MethodArgs::const_iterator it = args.begin();
-		int min_args = method->getMinNumArgs();
-		int num_args = method->getNumArgs();
-		std::string args_name;
-
-		if (min_args != num_args) {
-			method->setReference(0);
-		}
-
-		for (int n = 1; it != args.end(); ++it, ++n) {
-			args_name += it->second->getName()->str();
-			args_name += CLEVER_ARGS_SEPARATOR;
-			if (min_args != num_args && n >= min_args) {
-				m_methods[method->getName()].insert(MethodPair(args_name, method));
-				method->addRef();
-			}
-		}
-		if (min_args == num_args) {
-			m_methods[method->getName()].insert(MethodPair(args_name, method));
-		}
-	}
-
-	const Method* getMethod(const CString* name, const TypeVector* args) const {
-		MethodMap::const_iterator it = m_methods.find(*name);
-
-		if (it == m_methods.end()) return NULL;
-
-		std::string args_name;
-
-		if (args != NULL) {
-			for (size_t i = 0, j = args->size(); i < j; ++i) {
-				args_name += args->at(i)->getName()->str();
-				args_name += CLEVER_ARGS_SEPARATOR;
-			}
-		}
-
-		OverloadMethodMap::const_iterator method_it = it->second.find(args_name);
-
-		if (method_it != it->second.end()) {
-			return method_it->second;
-		}
-
-		return NULL;
-	}
+	const Method* getMethod(const CString*, const TypeVector*) const;
 
 	const CString* getName() const {
 		return m_name;
