ed39e7f6336eb3f089182a84f6086a73ad03f14a
Felipe Pena <felipensp@gmail.com>
2012-03-19 10:49:32 -0300

    - Fixed error reported by Valgrind (uninitialized ptr)

--- compiler/function.h
+++ compiler/function.h
@@ -69,35 +69,40 @@ public:
 
 	explicit Function(std::string name)
 		: m_name(name), m_kind(INTERNAL), m_num_args(0), m_min_args(0),
-			m_rtype(NULL) {}
+			m_rtype(NULL), m_scope(NULL) {}
 
-	Function(std::string libname, std::string name, const Type* rtype, FunctionPtr ptr)
-		: m_libname(libname), m_lfname(name), m_name(name), m_kind(EXTERNAL), m_num_args(0), m_min_args(0),
-			m_rtype(rtype) { m_info.ptr = ptr; }
+	Function(std::string libname, std::string name, const Type* rtype,
+		FunctionPtr ptr)
+		: m_libname(libname), m_lfname(name), m_name(name),
+			m_kind(EXTERNAL), m_num_args(0), m_min_args(0),
+			m_rtype(rtype), m_scope(NULL) { m_info.ptr = ptr; }
 
-	Function(std::string libname, std::string lfname, std::string name, const Type* rtype, FunctionPtr ptr)
-		: m_libname(libname), m_lfname(lfname), m_name(name), m_kind(EXTERNAL), m_num_args(0), m_min_args(0),
-			m_rtype(rtype) { m_info.ptr = ptr; }
+	Function(std::string libname, std::string lfname, std::string name,
+		const Type* rtype, FunctionPtr ptr)
+		: m_libname(libname), m_lfname(lfname), m_name(name),
+			m_kind(EXTERNAL), m_num_args(0), m_min_args(0),
+			m_rtype(rtype), m_scope(NULL) { m_info.ptr = ptr; }
 
 	Function(std::string name, FunctionPtr ptr)
 		: m_name(name), m_kind(INTERNAL), m_num_args(0), m_min_args(0),
-			m_rtype(NULL) { m_info.ptr = ptr; }
+			m_rtype(NULL), m_scope(NULL) { m_info.ptr = ptr; }
 
 	Function(std::string name, FunctionPtr ptr, const Type* rtype)
 		: m_name(name), m_kind(INTERNAL), m_num_args(0), m_min_args(0),
-			m_rtype(rtype) { m_info.ptr = ptr; }
+			m_rtype(rtype), m_scope(NULL) { m_info.ptr = ptr; }
 
-	Function(std::string name, FunctionPtr ptr, int numargs, const Type* rtype)
-		: m_name(name), m_kind(INTERNAL), m_num_args(numargs), m_min_args(0),
-			m_rtype(rtype) { m_info.ptr = ptr; }
+	Function(std::string name, FunctionPtr ptr, int numargs,
+		const Type* rtype)
+		: m_name(name), m_kind(INTERNAL), m_num_args(numargs),
+			m_min_args(0), m_rtype(rtype), m_scope(NULL) { m_info.ptr = ptr; }
 
 	Function(std::string& name, size_t offset)
-		: m_name(name), m_kind(USER), m_num_args(0), m_min_args(0), m_rtype(NULL)
-			{ m_info.offset = offset; }
+		: m_name(name), m_kind(USER), m_num_args(0), m_min_args(0),
+			m_rtype(NULL), m_scope(NULL) { m_info.offset = offset; }
 
 	Function(std::string& name, size_t offset, int numargs)
 		: m_name(name), m_kind(USER), m_num_args(numargs), m_min_args(0),
-			m_rtype(NULL) { m_info.offset = offset; }
+			m_rtype(NULL), m_scope(NULL) { m_info.offset = offset; }
 
 	virtual ~Function() {}
 
