d3b126c20b2e680c40764110d13eac3e174680b8
Felipe Pena <felipensp@gmail.com>
2012-12-30 16:14:33 -0200

    - Added copyright header

--- core/astdump.h
+++ core/astdump.h
@@ -1,3 +1,10 @@
+/**
+ * Clever programming language
+ * Copyright (c) Clever Team
+ *
+ * This file is distributed under the MIT license. See LICENSE for details.
+ */
+
 #ifndef CLEVER_AST_DUMP_H
 #define CLEVER_AST_DUMP_H
 
