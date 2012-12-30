63eeea1dabb17b1f59ed6c769552ea884c1e0582
Felipe Pena <felipensp@gmail.com>
2012-12-29 23:38:39 -0200

    - WS

--- core/vm.cc
+++ core/vm.cc
@@ -61,153 +61,149 @@ inline void VM::init()
 
 inline Value* VM::getValue(size_t scope_id, size_t value_id) const
 {
-    return (*m_scope_pool)[scope_id]->getValue(value_id);
+	return (*m_scope_pool)[scope_id]->getValue(value_id);
 }
 
 #ifdef CLEVER_DEBUG
 void VM::dumpOpcodes() const
 {
-    const char *op_type[] = {
-        "UNUSED", "FETCH_VAL", "JMP_ADDR"
-    };
+	const char *op_type[] = {"UNUSED", "FETCH_VAL", "JMP_ADDR"};
 
-    for (size_t i = 0, j = m_inst.size(); i < j; ++i) {
-        IR& ir = m_inst[i];
-        ::printf("[%03ld] %-12s | %3ld:%3ld (%-9s) | %3ld:%3ld (%-9s) | %p\n",
-            i,
-            get_opcode_name(ir.opcode),
-            ir.op1, ir.op1_scope, op_type[ir.op1_type],
-            ir.op2, ir.op2_scope, op_type[ir.op2_type],
-            ir.result);
-    }
+	for (size_t i = 0, j = m_inst.size(); i < j; ++i) {
+		IR& ir = m_inst[i];
+		::printf("[%03ld] %-12s | %3ld:%3ld (%-9s) | %3ld:%3ld (%-9s) | %p\n",
+			i, get_opcode_name(ir.opcode),
+			ir.op1, ir.op1_scope, op_type[ir.op1_type],
+			ir.op2, ir.op2_scope, op_type[ir.op2_type],	ir.result);
+	}
 }
 #endif
 
 // Return operation
 VM_HANDLER(ret)
 {
-    if (m_call_stack.size()) {
-        const StackFrame& frame = m_call_stack.top();
-        const Value* val = getValue(op.op1_scope, op.op1);
+	if (m_call_stack.size()) {
+		const StackFrame& frame = m_call_stack.top();
+		const Value* val = getValue(op.op1_scope, op.op1);
 
-        if (val) {
-            m_call_stack.top().ret_val->copy(getValue(op.op1_scope, op.op1));
-        }
-        m_call_stack.pop();
+		if (val) {
+			m_call_stack.top().ret_val->copy(getValue(op.op1_scope, op.op1));
+		}
+		m_call_stack.pop();
 
-        // Go back to the caller
-        VM_GOTO(frame.ret_addr);
-    } else {
-        // Terminates the execution
-        VM_GOTO(m_inst.size());
-    }
+		// Go back to the caller
+		VM_GOTO(frame.ret_addr);
+	} else {
+		// Terminates the execution
+		VM_GOTO(m_inst.size());
+	}
 }
 
 // JMP operation
 VM_HANDLER(jmp)
 {
-    VM_GOTO(op.op1);
+	VM_GOTO(op.op1);
 }
 
 // JMPZ operation
 VM_HANDLER(jmpz)
 {
-    Value* value = getValue(op.op1_scope, op.op1);
+	Value* value = getValue(op.op1_scope, op.op1);
 
-    if (!value->getInt()) {
-        VM_GOTO(op.op2);
-    }
+	if (!value->getInt()) {
+		VM_GOTO(op.op2);
+	}
 
-    VM_NEXT();
+	VM_NEXT();
 }
 
 // Assignment operation
 VM_HANDLER(assignment)
 {
-    Value* var = getValue(op.op1_scope, op.op1);
-    Value* value = getValue(op.op2_scope, op.op2);
+	Value* var = getValue(op.op1_scope, op.op1);
+	Value* value = getValue(op.op2_scope, op.op2);
 
-    var->copy(value);
+	var->copy(value);
 
-    VM_NEXT();
+	VM_NEXT();
 }
 
 // Math sum operation
 VM_HANDLER(add)
 {
-    Value* lhs = getValue(op.op1_scope, op.op1);
-    Value* rhs = getValue(op.op2_scope, op.op2);
+	Value* lhs = getValue(op.op1_scope, op.op1);
+	Value* rhs = getValue(op.op2_scope, op.op2);
 
-    if (lhs->getType() == CLEVER_INT_TYPE
-        && rhs->getType() == CLEVER_INT_TYPE) {
-        op.result->setInt(lhs->getInt() + rhs->getInt());
-    }
+	if (lhs->getType() == CLEVER_INT_TYPE
+		&& rhs->getType() == CLEVER_INT_TYPE) {
+		op.result->setInt(lhs->getInt() + rhs->getInt());
+	}
 
-    VM_NEXT();
+	VM_NEXT();
 }
 
 // Math subtraction operation
 VM_HANDLER(sub)
 {
-    Value* lhs = getValue(op.op1_scope, op.op1);
-    Value* rhs = getValue(op.op2_scope, op.op2);
+	Value* lhs = getValue(op.op1_scope, op.op1);
+	Value* rhs = getValue(op.op2_scope, op.op2);
 
-    if (lhs->getType() == CLEVER_INT_TYPE
-        && rhs->getType() == CLEVER_INT_TYPE) {
-        op.result->setInt(lhs->getInt() - rhs->getInt());
-    }
+	if (lhs->getType() == CLEVER_INT_TYPE
+		&& rhs->getType() == CLEVER_INT_TYPE) {
+		op.result->setInt(lhs->getInt() - rhs->getInt());
+	}
 
-    VM_NEXT();
+	VM_NEXT();
 }
 
 // Math multiplication operation
 VM_HANDLER(mul)
 {
-    Value* lhs = getValue(op.op1_scope, op.op1);
-    Value* rhs = getValue(op.op2_scope, op.op2);
+	Value* lhs = getValue(op.op1_scope, op.op1);
+	Value* rhs = getValue(op.op2_scope, op.op2);
 
-    if (lhs->getType() == CLEVER_INT_TYPE
-        && rhs->getType() == CLEVER_INT_TYPE) {
-        op.result->setInt(lhs->getInt() * rhs->getInt());
-    }
+	if (lhs->getType() == CLEVER_INT_TYPE
+		&& rhs->getType() == CLEVER_INT_TYPE) {
+		op.result->setInt(lhs->getInt() * rhs->getInt());
+	}
 
-    VM_NEXT();
+	VM_NEXT();
 }
 
 // Math division operation
 VM_HANDLER(div)
 {
-    Value* lhs = getValue(op.op1_scope, op.op1);
-    Value* rhs = getValue(op.op2_scope, op.op2);
+	Value* lhs = getValue(op.op1_scope, op.op1);
+	Value* rhs = getValue(op.op2_scope, op.op2);
 
-    if (lhs->getType() == CLEVER_INT_TYPE
-        && rhs->getType() == CLEVER_INT_TYPE) {
-        op.result->setInt(lhs->getInt() / rhs->getInt());
-    }
+	if (lhs->getType() == CLEVER_INT_TYPE
+		&& rhs->getType() == CLEVER_INT_TYPE) {
+		op.result->setInt(lhs->getInt() / rhs->getInt());
+	}
 
-    VM_NEXT();
+	VM_NEXT();
 }
 
 // Math modulus operation
 VM_HANDLER(mod)
 {
-    Value* lhs = getValue(op.op1_scope, op.op1);
-    Value* rhs = getValue(op.op2_scope, op.op2);
+	Value* lhs = getValue(op.op1_scope, op.op1);
+	Value* rhs = getValue(op.op2_scope, op.op2);
 
-    if (lhs->getType() == CLEVER_INT_TYPE
-        && rhs->getType() == CLEVER_INT_TYPE) {
-        op.result->setInt(lhs->getInt() % rhs->getInt());
-    }
+	if (lhs->getType() == CLEVER_INT_TYPE
+		&& rhs->getType() == CLEVER_INT_TYPE) {
+		op.result->setInt(lhs->getInt() % rhs->getInt());
+	}
 
-    VM_NEXT();
+	VM_NEXT();
 }
 
 // Receives values to be used in the next function call
 VM_HANDLER(send_val)
 {
-    m_call_args.push_back(getValue(op.op1_scope, op.op1));
+	m_call_args.push_back(getValue(op.op1_scope, op.op1));
 
-    VM_NEXT();
+	VM_NEXT();
 }
 
 // Saves function argument and local variables
@@ -256,28 +252,28 @@ void VM::restoreVars() const
 // Make a copy of VM instance
 void VM::copy(VM* vm)
 {
-     this->m_pc = vm->m_pc;
-     this->m_scope_pool = vm->m_scope_pool;
+	this->m_pc = vm->m_pc;
+	this->m_scope_pool = vm->m_scope_pool;
 
-     for (size_t i = 0; i < NUM_OPCODES; ++i) {
-         this->m_handlers[i] = vm->m_handlers[i];
-     }
+	for (size_t i = 0; i < NUM_OPCODES; ++i) {
+		this->m_handlers[i] = vm->m_handlers[i];
+	}
 
-     this->m_call_stack = vm->m_call_stack;
-     this->m_call_args = vm->m_call_args;
+	this->m_call_stack = vm->m_call_stack;
+	this->m_call_args = vm->m_call_args;
 }
 
 void VM::wait()
 {
-    for (size_t i = 0; i < m_thread_pool.size(); ++i) {
-        void* status;
+	for (size_t i = 0; i < m_thread_pool.size(); ++i) {
+		void* status;
 
-        pthread_join(m_thread_pool[i]->t_handler, &status);
+		pthread_join(m_thread_pool[i]->t_handler, &status);
 
-        delete m_thread_pool[i]->vm_handler;
-        delete m_thread_pool[i];
-    }
-    m_thread_pool.clear();
+		delete m_thread_pool[i]->vm_handler;
+		delete m_thread_pool[i];
+	}
+	m_thread_pool.clear();
 }
 
 void* thread_control(void* arg)
@@ -328,99 +324,98 @@ VM_HANDLER(threadcall)
 // Function call operation
 VM_HANDLER(fcall)
 {
-    Value* func = getValue(op.op1_scope, op.op1);
-    Function* fdata = static_cast<Function*>(func->getObj());
+	Value* func = getValue(op.op1_scope, op.op1);
+	Function* fdata = static_cast<Function*>(func->getObj());
 
-    if (fdata->isUserDefined()) {
-        if (m_call_stack.size()) {
-            saveVars();
-        }
-        m_call_stack.push(StackFrame());
-        m_call_stack.top().ret_addr = m_pc + 1;
-        m_call_stack.top().ret_val  = op.result;
+	if (fdata->isUserDefined()) {
+		if (m_call_stack.size()) {
+			saveVars();
+		}
+		m_call_stack.push(StackFrame());
+		m_call_stack.top().ret_addr = m_pc + 1;
+		m_call_stack.top().ret_val  = op.result;
 
-        // Function argument value binding
-        if (fdata->hasArgs()) {
-            Scope* arg_scope = fdata->getArgVars();
+		// Function argument value binding
+		if (fdata->hasArgs()) {
+			Scope* arg_scope = fdata->getArgVars();
 
-            m_call_stack.top().arg_vars = arg_scope;
+			m_call_stack.top().arg_vars = arg_scope;
 
-            for (size_t i = 0, j = arg_scope->size(); i < j; ++i) {
-                Value* arg_val = getValue(
-                    arg_scope->at(i).scope->getId(),
-                    arg_scope->at(i).value_id);
+			for (size_t i = 0, j = arg_scope->size(); i < j; ++i) {
+				Value* arg_val = getValue(
+					arg_scope->at(i).scope->getId(),
+					arg_scope->at(i).value_id);
 
-                arg_val->copy(m_call_args[i]);
-            }
-            m_call_args.clear();
-        }
-        VM_GOTO(fdata->getAddr());
-    } else {
-        fdata->getPtr()(m_call_args);
-        m_call_args.clear();
+				arg_val->copy(m_call_args[i]);
+			}
+			m_call_args.clear();
+		}
+		VM_GOTO(fdata->getAddr());
+	} else {
+		fdata->getPtr()(m_call_args);
+		m_call_args.clear();
 
-        VM_NEXT();
-    }
+		VM_NEXT();
+	}
 }
 
 // Leave operation
 VM_HANDLER(leave)
 {
-    const StackFrame& frame = m_call_stack.top();
+	const StackFrame& frame = m_call_stack.top();
 
-    if (m_call_stack.size() > 1) {
-        restoreVars();
-    }
+	if (m_call_stack.size() > 1) {
+		restoreVars();
+	}
 
-    m_call_stack.pop();
+	m_call_stack.pop();
 
-    // Go back to the next instruction after the caller
-    VM_GOTO(frame.ret_addr);
+	VM_GOTO(frame.ret_addr);
 }
 
 // Increment operation
 VM_HANDLER(inc)
 {
-    Value* value = getValue(op.op1_scope, op.op1);
+	Value* value = getValue(op.op1_scope, op.op1);
 
-    if (op.opcode == OP_PRE_INC) {
-        value->getType()->increment(value);
-        op.result->copy(value);
-    } else {
-        op.result->copy(value);
-        value->getType()->increment(value);
-    }
+	if (op.opcode == OP_PRE_INC) {
+		value->getType()->increment(value);
+		op.result->copy(value);
+	} else {
+		op.result->copy(value);
+		value->getType()->increment(value);
+	}
 
-    VM_NEXT();
+	VM_NEXT();
 }
 
 // Decrement operation
 VM_HANDLER(dec)
 {
-    Value* value = getValue(op.op1_scope, op.op1);
+	Value* value = getValue(op.op1_scope, op.op1);
 
-    if (op.opcode == OP_PRE_DEC) {
-        value->getType()->decrement(value);
-        op.result->copy(value);
-    } else {
-        op.result->copy(value);
-        value->getType()->decrement(value);
-    }
+	if (op.opcode == OP_PRE_DEC) {
+		value->getType()->decrement(value);
+		op.result->copy(value);
+	} else {
+		op.result->copy(value);
+		value->getType()->decrement(value);
+	}
 
-    VM_NEXT();
+	VM_NEXT();
 }
 
 // Executes the VM opcodes in a continuation-passing style
 void VM::run()
 {
-    // Loads the opcode handlers
-    init();
+	// Loads the opcode handlers
+	init();
 
-    for (size_t n = m_inst.size(); m_pc < n;) {
-        (this->*m_handlers[m_inst[m_pc].opcode])(m_inst[m_pc]);
-    }
+	for (size_t n = m_inst.size(); m_pc < n;) {
+		(this->*m_handlers[m_inst[m_pc].opcode])(m_inst[m_pc]);
+	}
 
-    wait();
+	wait();
 }
 
 } // clever
