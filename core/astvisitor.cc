79ba31de00f7ebc52a6fb0ae72e59e268ad3a889
Felipe Pena <felipensp@gmail.com>
2012-12-31 17:28:50 -0200

    - Fixed crash

--- core/astvisitor.cc
+++ core/astvisitor.cc
@@ -24,7 +24,9 @@ void Visitor::visit(Block* node) {
 
 void Visitor::visit(Assignment* node) {
 	node->getLhs()->accept(*this);
-	node->getRhs()->accept(*this);
+	if (node->getRhs()) {
+		node->getRhs()->accept(*this);
+	}
 }
 
 void Visitor::visit(VariableDecl* node) {
