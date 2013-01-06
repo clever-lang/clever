506a69e7738d91ea7a9bf687e32e9e1a68747cb0
krakjoe <joe.watkins@live.co.uk>
2013-01-06 17:05:01 +0000

    some more

--- modules/std/math/math.cc
+++ modules/std/math/math.cc
@@ -39,6 +39,59 @@ static CLEVER_FUNCTION(round) {
 	}
 }
 
+static CLEVER_FUNCTION(ceil) {
+	switch (CLEVER_ARG_COUNT()) {
+		case 1: {
+			if (CLEVER_ARG_TYPE(0)==CLEVER_INT_TYPE) {
+				//CLEVER_RETURN_INT(::std::ceil(CLEVER_ARG_INT(0)));
+			} else if (CLEVER_ARG_TYPE(0)==CLEVER_DOUBLE_TYPE) {
+				//CLEVER_RETURN_DBL(::std::ceil(CLEVER_ARG_DBL(0)));
+			} else {
+				Compiler::error("Math.ceil expected a double or integral value");
+			}
+		} break;
+		
+		default:
+			Compiler::error("Math.ceil expected exactly one argument");
+	}
+}
+
+static CLEVER_FUNCTION(floor) {
+	switch (CLEVER_ARG_COUNT()) {
+		case 1: {
+			if (CLEVER_ARG_TYPE(0)==CLEVER_INT_TYPE) {
+				//CLEVER_RETURN_INT(::std::floor(CLEVER_ARG_INT(0)));
+			} else if (CLEVER_ARG_TYPE(0)==CLEVER_DOUBLE_TYPE) {
+				//CLEVER_RETURN_DBL(::std::floor(CLEVER_ARG_DBL(0)));
+			} else {
+				Compiler::error("Math.floor expected a double or integral value");
+			}
+		} break;
+		
+		default:
+			Compiler::error("Math.floor expected exactly one argument");
+	}
+}
+
+static CLEVER_FUNCTION(abs) {
+	switch (CLEVER_ARG_COUNT()) {
+		case 1: {
+			if (CLEVER_ARG_TYPE(0)==CLEVER_INT_TYPE) {
+				//CLEVER_RETURN_INT(::std::labs(CLEVER_ARG_INT(0)));
+			} else if (CLEVER_ARG_TYPE(0)==CLEVER_DOUBLE_TYPE) {
+				//CLEVER_RETURN_DBL(::std::abs(CLEVER_ARG_DBL(0)));
+			} else {
+				Compiler::error("Math.floor expected a double or integral value");
+			}
+		} break;
+		
+		default:
+			Compiler::error("Math.floor expected exactly one argument");
+	}
+}
+
+
+
 } // math
 
 /**
@@ -49,6 +102,9 @@ CLEVER_MODULE_INIT(Math) {
 	
 	BEGIN_DECLARE_FUNCTION();
 	addFunction(new Function("round", &CLEVER_FUNC_NAME(round)));
+	addFunction(new Function("ceil", &CLEVER_FUNC_NAME(ceil)));
+	addFunction(new Function("floor", &CLEVER_FUNC_NAME(floor)));
+	addFunction(new Function("abs", &CLEVER_FUNC_NAME(abs)));
 	END_DECLARE();
 }
 
