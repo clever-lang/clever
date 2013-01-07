d21f2b182e9ab3a203259a06bec69661f6f377ab
krakjoe <joe.watkins@live.co.uk>
2013-01-07 11:01:39 +0000

    working ctor/dtor

--- modules/std/unicode/string.cc
+++ modules/std/unicode/string.cc
@@ -7,19 +7,38 @@
 #include "types/type.h"
 #include "modules/std/unicode/string.h"
 #include "unicode/unistr.h"
+#include "core/value.h"
 
 namespace clever { namespace packages { namespace std {
 
+#define CLEVER_UNICODE_STRING() (icu::UnicodeString*) CLEVER_THIS()->getObj()
+
 void* UnicodeString::allocData(CLEVER_TYPE_CTOR_ARGS) const {
+	if (args->size()) {
+		Value* from = args->at(0);
+		if (from) {
+			const CString* str = from->getStr();
+			if (str) {
+				return new icu::UnicodeString(
+					str->c_str(), str->size(), US_INV
+				);
+			}
+		}
+	}
 	return NULL;
 }
 
 void UnicodeString::deallocData(void *data) {
-	
+	delete (icu::UnicodeString*) data;
 }
 
 CLEVER_METHOD(UnicodeString::dbg) {
-	
+	if (CLEVER_THIS()) {
+		icu::UnicodeString* intern = CLEVER_UNICODE_STRING();	
+		if (intern) {
+			printf("Fetched UnicodeString from Object\n");
+		}
+	}
 }
 
 CLEVER_TYPE_OPERATOR(UnicodeString::add) {}
