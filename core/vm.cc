5f9c374cfa6b5f5572519f16d3d8e331fb2e01b7
Felipe Pena <felipensp@gmail.com>
2013-01-13 16:43:15 -0200

    - Cosmetics

--- core/vm.cc
+++ core/vm.cc
@@ -109,20 +109,13 @@ void VM::copy(const VM* vm)
 {
 	this->f_mutex = const_cast<VM*>(vm)->getMutex();
 	this->m_pc = vm->m_pc;
-
 	this->m_try_stack = vm->m_try_stack;
 
-	if (UNEXPECTED(vm->m_temp_env != NULL)) {
-		this->m_temp_env = new Environment(NULL);
-		this->m_temp_env->copy(vm->m_temp_env);
-	} else {
-		this->m_temp_env = NULL;
-	}
+	this->m_temp_env = new Environment(NULL);
+	this->m_temp_env->copy(vm->m_temp_env);
 
 	this->m_global_env = vm->m_global_env;
-
 	this->m_call_stack = vm->m_call_stack;
-
 	this->m_const_env = vm->m_const_env;
 }
 
