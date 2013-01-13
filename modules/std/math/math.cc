d57d688389fdc7e432c3bc3ced3e671f2415bb58
Felipe Pena <felipensp@gmail.com>
2013-01-13 13:00:33 -0200

    - CS

--- modules/std/math/math.cc
+++ modules/std/math/math.cc
@@ -21,64 +21,67 @@ namespace clever { namespace packages { namespace std {
 namespace math {
 
 // double round(double value)
-static CLEVER_FUNCTION(round) {
+static CLEVER_FUNCTION(round)
+{
 	if (!clever_static_check_args("d")) {
 		return;
 	}
 	double value = args[0]->getDouble();
 
 #ifdef CLEVER_MSVC
-	CLEVER_RETURN_DBL((value > 0.0) ? ::floor(value + 0.5) : ::ceil(value - 0.5));
+	result->setDouble((value > 0.0) ? ::floor(value + 0.5) : ::ceil(value - 0.5));
 #else
-	CLEVER_RETURN_DBL(::round(value));
+	result->setDouble(::round(value));
 #endif
 }
 
 // number ceil(number value)
-static CLEVER_FUNCTION(ceil) {
+static CLEVER_FUNCTION(ceil)
+{
 	if (!clever_static_check_args("n")) {
 		return;
 	}
 
 	if (args[0]->getType() == CLEVER_INT_TYPE) {
-		CLEVER_RETURN_INT(::std::ceil(args[0]->getInt()));
+		result->setInt(::std::ceil(args[0]->getInt()));
 	} else if (args[0]->getType() == CLEVER_DOUBLE_TYPE) {
-		CLEVER_RETURN_DBL(::std::ceil(args[0]->getDouble()));
+		result->setDouble(::std::ceil(args[0]->getDouble()));
 	}
 }
 
 // double floor(number value)
-static CLEVER_FUNCTION(floor) {
+static CLEVER_FUNCTION(floor)
+{
 	if (!clever_static_check_args("n")) {
 		return;
 	}
 
 	if (args[0]->getType() == CLEVER_INT_TYPE) {
-		CLEVER_RETURN_INT(::std::floor(args[0]->getInt()));
+		result->setInt(::std::floor(args[0]->getInt()));
 	} else if (args[0]->getType() == CLEVER_DOUBLE_TYPE) {
-		CLEVER_RETURN_DBL(::std::floor(args[0]->getDouble()));
+		result->setDouble(::std::floor(args[0]->getDouble()));
 	}
 }
 
 // number abs(number value)
-static CLEVER_FUNCTION(abs) {
+static CLEVER_FUNCTION(abs)
+{
 	if (!clever_static_check_args("n")) {
 		return;
 	}
 
 	if (args[0]->getType() == CLEVER_INT_TYPE) {
-		CLEVER_RETURN_INT(::std::labs(args[0]->getInt()));
+		result->setInt(::std::labs(args[0]->getInt()));
 	} else if (args[0]->getType() == CLEVER_DOUBLE_TYPE) {
-		CLEVER_RETURN_DBL(::std::abs(args[0]->getDouble()));
+		result->setDouble(::std::abs(args[0]->getDouble()));
 	}
 }
 
 } // clever::packages::std::math
 
-/**
- * Load module data
- */
-CLEVER_MODULE_INIT(Math) {
+// Load module data
+CLEVER_MODULE_INIT(Math)
+{
 	using namespace math;
 
 	addFunction(new Function("round", &CLEVER_FUNC_NAME(round)));
