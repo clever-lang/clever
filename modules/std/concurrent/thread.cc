c4aaadf8fe811fe0f9d3b365d3ab495e7beb9a16
krakjoe <joe.watkins@live.co.uk>
2013-01-16 15:06:17 +0000

    wierdness

--- modules/std/concurrent/thread.cc
+++ modules/std/concurrent/thread.cc
@@ -15,9 +15,11 @@
 namespace clever { namespace packages { namespace std {
 
 static inline void* ThreadHandler(void* ThreadArgument){
+	ThreadData* intern = static_cast<ThreadData*>(ThreadArgument);
+	
 	::std::cout << "Hello From ThreadHandler" << ::std::endl;
 	
-	return NULL;
+	return intern;
 }
 
 void Thread::dump(const void* data) const
@@ -41,17 +43,26 @@ void* Thread::allocData(CLEVER_TYPE_CTOR_ARGS) const
 
 		intern->thread = new pthread_t;
 		intern->lock = new pthread_mutex_t;
-
+		intern->entry = NULL;
+		
 		if (intern->lock) {
-			pthread_mutex_init(intern->lock, NULL);
+			if (pthread_mutex_init(intern->lock, NULL) != 0) {
+				clever_error("Thread.new failed to initialize a lock for the Thread");
+			}
 		}
 
 		if (args->size()) {
-			if (args->at(0)->getType() == CLEVER_FUNC_TYPE) {
-				intern->entry = static_cast<Function*>(args->at(0)->getObj());
+
+			Value* point = args->at(0);
+
+			/** there's something very wrong here **/
+			if (point->getType() == CLEVER_FUNC_TYPE) {
+				intern->entry = static_cast<Function*>(point->getObj());
 			} else {
-				clever_error("Thread.new was expecting a Function");
+				clever_error("Thread.new was expecting a Function and got something else");
 			}
+		} else {
+			clever_error("Thread.new was expecting a Function entry point and recieved no arguments");
 		}
 	}
 	return intern;
@@ -63,7 +74,10 @@ void Thread::deallocData(void* data)
 	if (intern) {
 
 		if (intern->lock) {
-			pthread_mutex_destroy(intern->lock);
+			if (pthread_mutex_destroy(intern->lock) != 0) {
+				clever_error("Thread.delete experienced an error destroying the Thread's lock");
+			}
+
 			delete intern->lock;
 		}
 		
@@ -81,10 +95,21 @@ CLEVER_METHOD(Thread::start)
 		return;
 	}
 	
-	if (intern->entry) {
+	if (intern->entry != NULL) {
 		/** @TODO(krakjoe) pthread attributes **/
-		result->setBool((pthread_create(intern->thread, NULL, ThreadHandler, intern) == 0));
-	} else result->setBool(false);
+
+		if (pthread_mutex_lock(intern->lock) == 0) {
+			result->setBool(
+				(pthread_create(intern->thread, NULL, ThreadHandler, intern) == 0)
+			);
+			pthread_mutex_unlock(intern->lock);
+		} else {
+			/* report fatality */
+			result->setBool(false);
+		}
+	} else {
+		result->setBool(false);
+	}
 }
 
 CLEVER_TYPE_INIT(Thread::init)
