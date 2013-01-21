06e504d2f8a5793569196bdaaceea214d748d7a5
Felipe Pena <felipensp@gmail.com>
2013-01-21 09:32:42 -0200

    - Constify

--- modules/std/crypto/md5.cc
+++ modules/std/crypto/md5.cc
@@ -348,16 +348,14 @@ void MD5::update(const char input[], size_type length)
 
 //////////////////////////////
 
- std::ostream& operator<<(std::ostream& out, MD5 md5)
+std::ostream& operator<<(std::ostream& out, MD5 md5)
 {
    return out << md5.hexdigest();
 }
 
 //////////////////////////////
 
- std::string md5(const std::string str)
+std::string md5(const std::string& str)
 {
-     MD5 md5 = MD5(str);
-
-     return md5.hexdigest();
+     return MD5(str).hexdigest();
 }
