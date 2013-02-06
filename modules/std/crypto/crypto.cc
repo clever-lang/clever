6cf52165158d441c9329b06bd96ed9d9c08d7370
gogo40 <pericles.raskolnikoff@gmail.com>
2013-02-06 18:51:08 -0300

    Base 64 added on crypto module

--- modules/std/crypto/crypto.cc
+++ modules/std/crypto/crypto.cc
@@ -7,6 +7,7 @@
 
 #include "modules/std/crypto/crypto.h"
 #include "modules/std/crypto/md5.h"
+#include "modules/std/crypto/base64.h"
 
 namespace clever { namespace packages { namespace std {
 
@@ -23,11 +24,32 @@ static CLEVER_FUNCTION(md5)
 	result->setStr(new StrObject(md5(*args[0]->getStr())));
 }
 
+static CLEVER_FUNCTION(base64Encode)
+{
+	if (!clever_static_check_args("s")) {
+		return;
+	}
+
+	result->setStr(new StrObject(base64Encode(*args[0]->getStr())));
+}
+
+
+static CLEVER_FUNCTION(base64Decode)
+{
+	if (!clever_static_check_args("s")) {
+		return;
+	}
+
+	result->setStr(new StrObject(base64Decode(*args[0]->getStr())));
+}
+
 } // clever::packages::std::crypto
 
 CLEVER_MODULE_INIT(CryptoModule)
 {
 	addFunction(new Function("md5", &CLEVER_NS_FNAME(crypto, md5)));
+	addFunction(new Function("base64_encode", &CLEVER_NS_FNAME(crypto, base64Encode)));
+	addFunction(new Function("base64_decode", &CLEVER_NS_FNAME(crypto, base64Decode)));
 }
 
 }}} // clever::packages::std
