ba54e39b240d287a21eebb4e39844d62dc2dc1fc
Felipe Pena <felipensp@gmail.com>
2013-02-12 21:56:28 -0200

    - Make regex module working again

--- modules/std/regex/pcre.cc
+++ modules/std/regex/pcre.cc
@@ -1,57 +1,43 @@
 /**
  * Clever programming language
- * Copyright (c) 2011-2012 Clever Team
+ * Copyright (c) Clever Team
  *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- *
+ * This file is distributed under the MIT license. See LICENSE for details.
  */
 
-#include "compiler/scope.h"
+#include "core/value.h"
+#include "modules/std/core/function.h"
 #include "modules/std/regex/pcre.h"
-#include "modules/std/regex/pcrevalue.h"
 
-namespace clever { namespace packages { namespace std { namespace regex {
+namespace clever { namespace modules { namespace std { namespace regex {
 
-/**
- * Regex(String pattern)
- */
-CLEVER_METHOD(Pcre::constructor) {
-	PcreValue* self = new PcreValue();
+// Regex.Regex(String pattern)
+CLEVER_METHOD(Pcre::constructor)
+{
+	if (!clever_check_args("s|i")) {
+		return;
+	}
+	PcreObject* self = static_cast<PcreObject*>(allocData(&args));
 
-	if (CLEVER_NUM_ARGS() == 2) {
-		self->re = new pcrecpp::RE(CLEVER_ARG_STR(0).c_str(),
-			pcrecpp::RE_Options(CLEVER_ARG_INT(1)));
+	if (args.size() == 2) {
+		self->re = new pcrecpp::RE(args.at(0)->getStr()->c_str(),
+			pcrecpp::RE_Options(args.at(1)->getInt()));
 	} else {
-		self->re = new pcrecpp::RE(CLEVER_ARG_STR(0).c_str());
+		self->re = new pcrecpp::RE(args.at(0)->getStr()->c_str());
 	}
 
-	CLEVER_RETURN_DATA_VALUE(self);
+	result->setObj(this, self);
 }
 
-/**
- * Bool Regex::matches(String haystack)
- */
-CLEVER_METHOD(Pcre::matches) {
-	PcreValue* self = CLEVER_GET_VALUE(PcreValue*, value);
-	const CString* haystack = CLEVER_ARG(0)->getStringP();
+// Bool Regex::matches(String haystack)
+CLEVER_METHOD(Pcre::matches)
+{
+	if (!clever_check_args("s")) {
+		return;
+	}
+
+	PcreObject* self = CLEVER_GET_OBJECT(PcreObject*, CLEVER_THIS());
+	const CString* haystack = args.at(0)->getStr();
 	int n;
 
 	// Check if we are testing another input, if not we just go to the next
@@ -61,13 +47,13 @@ CLEVER_METHOD(Pcre::matches) {
 
 		// Non capturing subpattern uses the partial match method
 		if (n == 0) {
-			CLEVER_RETURN_BOOL(self->re->PartialMatch(haystack->c_str()));
+			result->setBool(self->re->PartialMatch(haystack->c_str()));
 			return;
 		} else if (n > 0 && self->match.groups == NULL) {
 			self->match.n_groups = n;
 			self->match.matches = new ::std::string[n];
 			self->match.groups = new pcrecpp::Arg*[n];
-			self->match.input = haystack->str();
+			self->match.input = haystack->c_str();
 			self->match.last_input = haystack;
 
 			for (int i = 0; i < n; ++i) {
@@ -80,126 +66,93 @@ CLEVER_METHOD(Pcre::matches) {
 	}
 
 	int consumed;
-	bool result = self->re->DoMatch(self->match.input,
+	bool res = self->re->DoMatch(self->match.input,
 			pcrecpp::RE::UNANCHORED, &consumed, self->match.groups,	n);
 
-	if (result) {
+	if (res) {
 		self->match.input.remove_prefix(consumed);
 	}
 
-	CLEVER_RETURN_BOOL(result);
+	result->setBool(res);
 }
 
-/**
- * String Pcre::group(Int group)
- */
-CLEVER_METHOD(Pcre::group) {
-	PcreValue* self = CLEVER_GET_VALUE(PcreValue*, value);
+// String Pcre::group(Int group)
+CLEVER_METHOD(Pcre::group)
+{
+	if (!clever_check_args("i")) {
+		return;
+	}
 
-	if (CLEVER_ARG_INT(0) >= self->match.n_groups) {
-		CLEVER_RETURN_EMPTY_STR();
+	PcreObject* self = CLEVER_GET_OBJECT(PcreObject*, CLEVER_THIS());
+
+	if (args.at(0)->getInt() >= self->match.n_groups) {
+		result->setStr(CSTRING(""));
 	} else {
-		CLEVER_RETURN_STR(CSTRINGT(self->match.matches[CLEVER_ARG_INT(0)]));
+		result->setStr(new StrObject(self->match.matches[args.at(0)->getInt()]));
 	}
 }
 
-/**
- * String Regex::replace(String replacement, String haystack)
- */
-CLEVER_METHOD(Pcre::replace) {
-	PcreValue* self = CLEVER_GET_VALUE(PcreValue*, value);
-	::std::string newstr(CLEVER_ARG(1)->getStringP()->str());
+// String Regex::replace(String replacement, String haystack)
+CLEVER_METHOD(Pcre::replace)
+{
+	if (!clever_check_args("ss")) {
+		return;
+	}
 
-	self->re->Replace(CLEVER_ARG_STR(0), &newstr);
+	PcreObject* self = CLEVER_GET_OBJECT(PcreObject*, CLEVER_THIS());
 
-	CLEVER_RETURN_STR(CSTRINGT(newstr));
+	::std::string newstr(*args[1]->getStr());
+
+	self->re->Replace(args[0]->getStr()->c_str(), &newstr);
+
+	result->setStr(new StrObject(newstr));
 }
 
-/**
- * String Regex::replaceAll(String replacement, String haystack)
- */
-CLEVER_METHOD(Pcre::replaceAll) {
-	PcreValue* self = CLEVER_GET_VALUE(PcreValue*, value);
-	::std::string newstr(CLEVER_ARG(1)->getStringP()->str());
+// String Regex::replaceAll(String replacement, String haystack)
+CLEVER_METHOD(Pcre::replaceAll)
+{
+	if (!clever_check_args("ss")) {
+		return;
+	}
 
-	self->re->GlobalReplace(CLEVER_ARG_STR(0), &newstr);
+	PcreObject* self = CLEVER_GET_OBJECT(PcreObject*, CLEVER_THIS());
+	::std::string newstr(args[1]->getStr()->c_str());
 
-	CLEVER_RETURN_STR(CSTRINGT(newstr));
+	self->re->GlobalReplace(args[0]->getStr()->c_str(), &newstr);
+
+	result->setStr(new StrObject(newstr));
 }
 
-/**
- * Void Regex::operator=(Regex regex)
- */
-CLEVER_METHOD(Pcre::do_assign) {
-	CLEVER_THIS()->copy(CLEVER_ARG(0));
+// String Regex::quote(String regex)
+CLEVER_METHOD(Pcre::quote)
+{
+	if (!clever_static_check_args("s")) {
+		return;
+	}
+	result->setStr(new StrObject(pcrecpp::RE::QuoteMeta(args[0]->getStr()->c_str())));
 }
 
-/**
- * String Regex::quote(String regex)
- */
-CLEVER_METHOD(Pcre::quote) {
-	CLEVER_RETURN_STR(CSTRING(pcrecpp::RE::QuoteMeta(CLEVER_ARG_STR(0))));
+CLEVER_TYPE_INIT(Pcre::init)
+{
+	Function* ctor = new Function("Regex", (MethodPtr)&Pcre::constructor);
+
+	// Methods
+	setConstructor(ctor);
+	addMethod(ctor);
+	addMethod(new Function("matches",    (MethodPtr)&Pcre::matches));
+	addMethod(new Function("group",      (MethodPtr)&Pcre::group));
+	addMethod(new Function("replace",    (MethodPtr)&Pcre::replace));
+	addMethod(new Function("replaceAll", (MethodPtr)&Pcre::replaceAll));
+
+	// Static methods
+	addMethod(new Function("quote",      (MethodPtr)&Pcre::quote))->setStatic();
+
+	// Constants
+	addProperty(CSTRING("DOTALL"),    new Value(long(PCRE_DOTALL),    true));
+	addProperty(CSTRING("MULTILINE"), new Value(long(PCRE_MULTILINE), true));
+	addProperty(CSTRING("CASELESS"),  new Value(long(PCRE_CASELESS),  true));
+	addProperty(CSTRING("UTF8"),      new Value(long(PCRE_UTF8),      true));
+	addProperty(CSTRING("UNGREEDY"),  new Value(long(PCRE_UNGREEDY),  true));
 }
 
-void Pcre::destructor(Value* value) const {
-
-}
-
-DataValue* Pcre::allocateValue() const {
-	return new PcreValue();
-}
-
-void Pcre::init() {
-	const Type* type = g_regex_type;
-
-	clever_assert_not_null(type);
-
-	// Regex(String pattern [, Int options])
-	addMethod(
-		(new Method(CLEVER_CTOR_NAME, (MethodPtr)&Pcre::constructor, type))
-		->addArg("pattern", CLEVER_STR)
-		->addArg("options", CLEVER_INT)
-		->setMinNumArgs(1)
-	);
-
-	// Bool Regex::matches(String haystack)
-	addMethod(
-		(new Method("matches", (MethodPtr)&Pcre::matches, CLEVER_BOOL))
-		->addArg("haystack", CLEVER_STR)
-	);
-
-	// String Regex::group(Int group)
-	addMethod(
-		(new Method("group", (MethodPtr)&Pcre::group, CLEVER_STR))
-		->addArg("group", CLEVER_INT)
-	);
-
-	// Bool Regex::replace(String replacement, String haystack)
-	addMethod(
-		(new Method("replace", (MethodPtr)&Pcre::replace, CLEVER_BOOL))
-		->addArg("replacement", CLEVER_STR)
-		->addArg("haystack", CLEVER_STR)
-	);
-
-	// Bool Regex::replaceAll(String replacement, String haystack)
-	addMethod(
-		(new Method("replaceAll", (MethodPtr)&Pcre::replaceAll, CLEVER_BOOL))
-		->addArg("replacement", CLEVER_STR)
-		->addArg("haystack", CLEVER_STR)
-	);
-
-	// Void Regex::operator= (Regex regex)
-	addMethod(
-		(new Method(CLEVER_OPERATOR_ASSIGN, (MethodPtr)&Pcre::do_assign, CLEVER_VOID))
-		->addArg("rvalue", type)
-	);
-
-	// String Regex::quote(String regex)
-	addMethod(
-		(new Method("quote", (MethodPtr)&Pcre::quote, CLEVER_STR))
-		->setStatic()
-		->addArg("regex", CLEVER_STR)
-	);
-}
-
-}}}} // clever::packages:std::regex
+}}}} // clever::modules:std::regex
--- modules/std/regex/pcre.h
+++ modules/std/regex/pcre.h
@@ -1,63 +1,91 @@
 /**
  * Clever programming language
- * Copyright (c) 2011-2012 Clever Team
+ * Copyright (c) Clever Team
  *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
+ * This file is distributed under the MIT license. See LICENSE for details.
  */
 
 #ifndef CLEVER_STD_REGEX_PCRE_H
 #define CLEVER_STD_REGEX_PCRE_H
 
-#include "compiler/module.h"
-#include "compiler/value.h"
+#include <pcrecpp.h>
 #include "types/type.h"
 
-namespace clever { namespace packages { namespace std { namespace regex {
+namespace clever { namespace modules { namespace std { namespace regex {
 
-#define REGEX_TYPE_NAME "Regex"
+struct PcreMatch {
+public:
+	PcreMatch()
+		: last_input(NULL), groups(NULL), matches(NULL), n_groups(0) {}
+
+	~PcreMatch() {
+		if (groups == NULL) {
+			return;
+		}
+		delete[] matches;
+
+		for (int i = 0; i < n_groups; ++i) {
+			delete groups[i];
+		}
+
+		delete[] groups;
+	}
+
+	const CString* last_input;
+	pcrecpp::Arg** groups;
+	::std::string* matches;
+	int n_groups;
+	pcrecpp::StringPiece input;
+private:
+	DISALLOW_COPY_AND_ASSIGN(PcreMatch);
+};
+
+struct PcreObject : public TypeObject {
+public:
+	PcreObject()
+		: re(NULL) {}
+
+	virtual ~PcreObject() {
+		if (re) {
+			delete re;
+		}
+	}
+
+	pcrecpp::RE* re;
+	PcreMatch match;
+private:
+	DISALLOW_COPY_AND_ASSIGN(PcreObject);
+};
 
 class Pcre : public Type {
 public:
-	Pcre() : Type(CSTRING(REGEX_TYPE_NAME)) {}
+	Pcre()
+		: Type("Regex") {}
 
 	void init();
-	DataValue* allocateValue() const;
-	void destructor(Value* value) const;
+
+	TypeObject* allocData(CLEVER_TYPE_CTOR_ARGS) const { return new PcreObject; }
+
+	void deallocData(void* data) {
+		if (data) {
+			delete static_cast<PcreObject*>(data);
+		}
+	}
 
 	// Methods
-	static CLEVER_METHOD(constructor);
-	static CLEVER_METHOD(matches);
-	static CLEVER_METHOD(group);
-	static CLEVER_METHOD(replace);
-	static CLEVER_METHOD(replaceAll);
-	static CLEVER_METHOD(do_assign);
+	CLEVER_METHOD(constructor);
+	CLEVER_METHOD(matches);
+	CLEVER_METHOD(group);
+	CLEVER_METHOD(replace);
+	CLEVER_METHOD(replaceAll);
+	CLEVER_METHOD(do_assign);
 
 	// Static methods
-	static CLEVER_METHOD(quote);
+	CLEVER_METHOD(quote);
 private:
 	DISALLOW_COPY_AND_ASSIGN(Pcre);
 };
 
-extern Type* g_regex_type;
-
-}}}} // clever::packages::std::regex
+}}}} // clever::modules::std::regex
 
 #endif
