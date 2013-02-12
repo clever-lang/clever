ccdef17e9de1f9d6e094a9026820c42c8a67e997
Felipe Pena <felipensp@gmail.com>
2013-02-12 17:13:22 -0200

    - Clean up

--- core/value.cc
+++ core/value.cc
@@ -15,7 +15,6 @@ namespace clever {
 
 void Value::copy(const Value* value)
 {
-	SAFETY_LOCK();
 	cleanUp();
 	m_type = value->getType();
 	m_data = value->getData();
@@ -23,7 +22,6 @@ void Value::copy(const Value* value)
 	if (EXPECTED(m_type && m_data)) {
 		m_data->addRef();
 	}
-	SAFETY_ULOCK();
 }
 
 bool Value::asBool() const
@@ -38,66 +36,57 @@ bool Value::asBool() const
 
 void Value::setInt(long n)
 {
-	SAFETY_LOCK();
 	cleanUp();
 	m_type = CLEVER_INT_TYPE;
 	setObj(m_type, new IntObject(n));
-	SAFETY_ULOCK();
 }
 
 long Value::getInt() const
 {
-	SAFETY_GET(long, IntObject);
+	return static_cast<IntObject*>(getObj())->value;
 }
 
 void Value::setDouble(double n)
 {
-	SAFETY_LOCK();
 	cleanUp();
 	m_type = CLEVER_DOUBLE_TYPE;
 	setObj(m_type, new DoubleObject(n));
-	SAFETY_ULOCK();
 }
 
 double Value::getDouble() const
 {
-	SAFETY_GET(double, DoubleObject);
+	return static_cast<DoubleObject*>(getObj())->value;
 }
 
 void Value::setStr(const CString* str)
 {
-	SAFETY_LOCK();
 	cleanUp();
 	m_type = CLEVER_STR_TYPE;
 	setObj(m_type, new StrObject(str));
-	SAFETY_ULOCK();
 }
 
-void Value::setStr(StrObject* str) {
-	SAFETY_LOCK();
+void Value::setStr(StrObject* str)
+{
 	cleanUp();
 	m_type = CLEVER_STR_TYPE;
 	setObj(m_type, str);
-	SAFETY_ULOCK();
 }
 
 const CString* Value::getStr() const
 {
-	SAFETY_GET(const CString*, StrObject);
+	return static_cast<StrObject*>(getObj())->value;
 }
 
 void Value::setBool(bool n)
 {
-	SAFETY_LOCK();
 	cleanUp();
 	m_type = CLEVER_BOOL_TYPE;
 	setObj(m_type, new BoolObject(n));
-	SAFETY_ULOCK();
 }
 
 bool Value::getBool() const
 {
-	SAFETY_GET(bool, BoolObject);
+	return static_cast<BoolObject*>(getObj())->value;
 }
 
 } // clever
