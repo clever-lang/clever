73b5c8a6373259e03708ce3f829b81af2b5e1b3f
Higor Eurípedes <heuripedes@gmail.com>
2013-02-12 20:43:47 -0200

    - Added clever_add_lib macro to FindCLEVER.cmake

--- cmake/FindCLEVER.cmake
+++ cmake/FindCLEVER.cmake
@@ -88,3 +88,78 @@ macro(clever_check_mod_env Name)
 	
 endmacro(clever_check_mod_env)
 
+# clever_add_lib(<var_name>
+#       [PKGS pkg1 [pkg2 ...]]
+#       [LIBS lib1 [lib2 ...]]
+#       [INCS header1 [header2 ...]]
+#       [REQUIRED]
+# 	)
+#
+# PKGS is a list of pkg-config modules.
+#
+# LIBS is a list of libraries (without the `lib` prefix or the `.ext` suffix)
+#
+# INCS is a list of files to look for in include directories.
+#
+# YOU MUST SPECIFY AT LEAST ONE OF THE ABOVE OPTIONS.
+#
+# When REQUIRED is set, the macro will exit with an error.
+#
+macro(clever_add_lib VarName)
+	set(options REQUIRED)
+	set(multiValueArgs PKGS LIBS INCS)
+
+	cmake_parse_arguments(_add_lib "${options}" "" "${multiValueArgs}" ${ARGN})
+
+	set(_add_lib_found FALSE)
+
+	# automated checking with pkg-config
+	if (NOT ${VarName}_DIR)
+		if (PKG_CONFIG_FOUND AND _add_lib_PKGS)
+			pkg_check_modules(${VarName} ${_add_lib_PKGS})
+		endif (PKG_CONFIG_FOUND AND _add_lib_PKGS)
+	endif (NOT ${VarName}_DIR)
+
+	# fallback to manual checking
+	if (NOT ${VarName}_FOUND)
+		set(_inc_path "")
+		set(_lib_path "")
+
+		if (${VarName}_DIR)
+			set(_inc_path "${VarName}/include")
+			set(_lib_path "${VarName}/lib")
+		endif (${VarName}_DIR)
+
+		if (_add_lib_INCS)
+			find_path(_path NAMES ${_add_lib_INCS} PATHS "${_inc_path}")
+			if (_path)
+				set(${VarName}_INCLUDE_DIRS ${_path})
+			else (_path)
+				message(WARNING "Failed to find include directories for `${_add_lib_INCS}`.")
+			endif (_path)
+		endif (_add_lib_INCS)
+		if (_add_lib_LIBS)
+			foreach (_name ${_add_lib_LIBS})
+				find_library(_lib${_name} NAMES "${_name}" PATHS "${_lib_path}")
+
+				if (_lib${_name})
+					list(APPEND ${VarName}_LIBRARIES ${_lib${_name}})
+				else (_lib${_name})
+					if (_add_lib_REQUIRED)
+						message(SEND_ERROR "Failed to find required library `${_name}`.")
+					endif (_add_lib_REQUIRED)
+					break()
+				endif (_lib${_name})
+
+				unset(_lib${_name})
+			endforeach (_name)
+			unset(_name)
+		endif (_add_lib_LIBS)
+
+		if (${VarName}_LIBRARIES)
+			set(${VarName}_FOUND TRUE)
+		endif (${VarName}_LIBRARIES)
+	endif (NOT ${VarName}_FOUND)
+
+endmacro(clever_add_lib)
+
