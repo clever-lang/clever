a30252aa7ce46933688ed2c29940ae4dc9fff684
gogo40 <pericles.raskolnikoff@gmail.com>
2013-02-16 23:52:23 -0300

    python multi-thread test

--- benchmark/threads/threads_001.py
+++ benchmark/threads/threads_001.py
@@ -1,22 +1,48 @@
 import timeit
+from multiprocessing import Process, Queue
+
+def process(r, ini, fim):
+	acc = 0
+	i = ini
+
+	while (i <= fim):
+		acc = acc + i
+		i = i+1
+
+	r.put(acc)
 
 
+if __name__ == '__main__':
 
-t0 = timeit.default_timer()
+	t0 = timeit.default_timer()
 
-acc = 0
-i = 0
-nthreads = 2
-n = nthreads * 10000000
+	nthreads = 2
+	L = 10000000
+	n = nthreads * L
+	acc = Queue()
+	process(acc, 0, n)
+
+	t1 = timeit.default_timer()
+
+	print "Time elapsed single-thread python: ", t1 - t0
+	print "Result: ", acc.get()
 
 
-while (i <= n):
-	acc = acc + i
-	i = i+1
+	t2 = timeit.default_timer()
 
-print acc
+	acc1 = Queue()
+	acc2 = Queue()
 
-t1 = timeit.default_timer()
+	p1 = Process(target=process, args=(acc1, 0, L))
+	p2 = Process(target=process, args=(acc2, L + 1, 2 * L))
+	p1.start()
+	p2.start()
+	p1.join()
+	p2.join()
 
-print "Time elapsed : ", t1 - t0
+	t3 = timeit.default_timer()
 
+	print "Time elapsed multi-thread python: ", t3 - t2
+	print "Result: ", acc1.get() + acc2.get()
+
+	
