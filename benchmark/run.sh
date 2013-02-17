e5dd740375125b539a5dac38a629650dae7864c4
gogo40 <pericles.raskolnikoff@gmail.com>
2013-02-17 01:59:14 -0300

    C version to benchmark added

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -10,11 +10,16 @@ echo "threads/threads_001.py: [python version]"
 python threads/threads_001.py
 echo "[OK]"
 
-echo "threads/threads_001.lua: [lua version"
+echo "threads/threads_001.lua: [lua version]"
 lua threads/threads_001.lua
 echo "[OK]"
 
 echo "threads/threads_001.php: [php version]"
 php threads/threads_001.php
+echo "[OK]"
+
+echo "threads/threads_001.c: [C version]"
+gcc -O2 -o threads/threads_001.exe threads/threads_001.c
+threads/threads_001.exe
 echo "[OK]"
 
