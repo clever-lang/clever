af4ffb6ec43af028ae3471ee819033b1fa795822
Felipe Pena <felipensp@gmail.com>
2013-02-17 01:31:56 -0300

    - php benchmark test

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -7,7 +7,10 @@ echo "threads/threads_001.clv:"
 echo "[OK]"
 
 echo "threads/threads_001.py: [python version]"
- 
 python threads/threads_001.py
+echo "[OK]"
+
+echo "threads/threads_001.php: [php version]"
+php threads/threads_001.php
 echo "[OK]"
 
