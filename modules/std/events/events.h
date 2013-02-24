acf77b4dc6b4b59b267154ada3b4672050ba347b
gogo40 <pericles.raskolnikoff@gmail.com>
2013-02-24 16:12:31 -0300

    Events is working now

--- modules/std/events/events.h
+++ modules/std/events/events.h
@@ -9,14 +9,46 @@
 #define CLEVER_STD_EVENTS_H
 
 #include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <queue>
 
 #include "core/cstring.h"
 #include "types/type.h"
 #include "core/module.h"
+#include "core/cthread.h"
+#include "core/vm.h"
 
 namespace clever { namespace modules { namespace std {
 
+
+typedef ::std::vector<Function*> Actions;
+typedef ::std::map<CString, Actions> EventMap;
+
+typedef ::std::vector<CString, int> Requisitions;
+typedef ::std::pair<Requisitions, Function*> RequisitionActionPair;
+typedef ::std::vector<RequisitionActionPair> RequisitionActionPairs;
+typedef ::std::map<int, RequisitionActionPairs> RequisitionSubMap;
+typedef ::std::map<CString, RequisitionSubMap> RequistionMap;
+
+typedef ::std::vector<Value*> ActionArgs;
+typedef ::std::pair<CString, ActionArgs> Signal;
+typedef ::std::queue<Signal> EventsQueue;
+
+
 struct EventData : public TypeObject {
+	EventMap m_event_map;
+	EventsQueue m_event_queue;
+
+	CThread handler;
+	CMutex mutex;
+
+	VM* m_vm;
+
+	int m_sleep_time;
+
 	EventData() {}
 
 	~EventData();
@@ -33,6 +65,10 @@ public:
 	virtual TypeObject* allocData(CLEVER_TYPE_CTOR_ARGS) const;
 
 	CLEVER_METHOD(ctor);
+	CLEVER_METHOD(connect);
+	CLEVER_METHOD(emmit);
+	CLEVER_METHOD(wait);
+	CLEVER_METHOD(finalize);
 };
 
 class EventsModule : public Module {
