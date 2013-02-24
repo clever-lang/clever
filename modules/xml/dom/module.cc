8ed1e21e31eca505f40eea7fb29547d038413165
Felipe Pena <felipensp@gmail.com>
2013-02-24 17:16:44 -0300

    - Initial code for xml.dom module

---
+++ modules/xml/dom/module.cc
@@ -0,0 +1,17 @@
+/**
+ * Clever programming language
+ * Copyright (c) Clever Team
+ *
+ * This file is distributed under the MIT license. See LICENSE for details.
+ */
+
+#include "modules/xml/dom/module.h"
+
+namespace clever { namespace modules { namespace xml {
+
+CLEVER_MODULE_INIT(DOMXMLModule)
+{
+
+}
+
+}}} // clever::modules::xml
