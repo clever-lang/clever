549448841c2eda7b411b9af826e9c274b73208bf
gogo40 <pericles.raskolnikoff@gmail.com>
2013-03-02 18:49:09 -0300

    wild X with class isn't running

--- resources/clever_gmp/gmp.sh
+++
@@ -1,9 +0,0 @@
-#A script to build a shared library to test FFI support on Clever.
-gcc -fPIC -O3 -shared -c gmp.c
-gcc -shared -Wl,-soname,gmp.so -o gmp.so gmp.o -lc -ldl -lgmp
-
-
-set LD_LIBRARY_PATH=.
-
-./clever  gmp.clv
-
--- resources/rpc_test/run_rpc_test.sh
+++
@@ -1,7 +0,0 @@
-./clever client_rpc.clv > t1 &
-./clever client_rpc.clv > t2 &
-./clever client_rpc.clv > t3 &
-./clever client_rpc.clv > t4 &
-./clever client_rpc.clv > t5 &
-
-
